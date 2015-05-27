0cc8add3423b4b710d9034a431dbbf7d86a1707d
bartwo <wouters.bart@cegeka.be>
2015-05-27 16:43:53 +0200

    Added vhost access restrictions functionality

---
+++ manifests/vhost_access_restriction.pp
@@ -0,0 +1,61 @@
+/*
+
+== Definition: apache::vhost-access-restriction
+
+Simple way of restriction the access to a folder in a given virtualhost.
+
+This definition will ensure all the required modules are loaded and will
+drop a configuration snippet in the virtualhost's conf/ directory.
+
+Parameters:
+- *ensure*: present/absent.
+- *location*: path in virtualhost's context to pass through using the ProxyPass
+  directive.
+- *url*: destination to which the ProxyPass directive points to.
+- *params*: a table of key=value (min, max, timeout, retry, etc.) described
+  in the ProxyPass Directive documentation http://httpd.apache.org/docs/current/mod/mod_proxy.html#proxypass
+- *vhost*: the virtualhost to which this directive will apply. Mandatory.
+- *filename*: basename of the file in which the directive(s) will be put.
+  Useful in the case directive order matters: apache reads the files in conf/
+  in alphabetical order.
+- *sslbackend*: define wether the module should enable proxy ssl backends
+- *proxy_config*: addition proxy configuration like ProxyPreserveHost, ..
+
+Requires:
+- Class["apache"]
+- matching Apache::Vhost[] instance
+
+Example usage:
+
+  apache::proxypass { "proxy legacy dir to legacy server":
+    ensure       => present,
+    location     => "/legacy/",
+    url          => "http://legacyserver.example.com",
+    params       => ["retry=5", "ttl=120"],
+    vhost        => "www.example.com",
+    proxy_config => ['ProxyPreserveHost On']
+  }
+
+*/
+define apache::vhost_access_restriction (
+  $vhost=namevar,
+  $ensure='present',
+  $folder,
+  $options='FollowSymLinks',
+  $order='Deny,Allow',
+  $deny_from = [],
+  $allow_from = [],
+) {
+
+  include apache::params
+
+
+  file { "access restriction on ${name} on ${vhost}":
+    #file { "access restictions ${name} on ${vhost}":
+    ensure  => $ensure,
+    content => template('apache/vhost_access_restriction.erb'),
+    path    => "${apache::params::conf}/conf.d/00-default-${vhost}.conf",
+    notify  => Exec['apache-graceful'],
+    require => Apache::Vhost[$vhost],
+  }
+}
